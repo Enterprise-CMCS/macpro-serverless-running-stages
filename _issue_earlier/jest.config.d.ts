1099692287b091e3ea287e00dcc712849be0822d
Jon Holman <9025884+JonHolman@users.noreply.github.com>
2023-01-10 16:08:46 -0500

    wip

---
+++ _issue_earlier/jest.config.d.ts
@@ -0,0 +1,14 @@
+declare const _default: {
+    moduleDirectories: string[];
+    moduleFileExtensions: string[];
+    roots: string[];
+    testEnvironment: string;
+    testMatch: string[];
+    testPathIgnorePatterns: string[];
+    coveragePathIgnorePatterns: string[];
+    transform: {
+        "^.+\\.(ts|tsx)$": string;
+        "^.+\\.(png|svg)$": string;
+    };
+};
+export default _default;
