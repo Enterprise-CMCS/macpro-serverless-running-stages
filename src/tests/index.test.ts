44522e99400a33da893ed2aa8a43b1729520a85e
Jon Holman <9025884+JonHolman@users.noreply.github.com>
2023-01-11 13:38:42 -0500

    started working on unit tests

--- src/tests/index.test.ts
+++ src/tests/index.test.ts
@@ -1,12 +1,60 @@
-import { it, describe, expect, vi } from "vitest";
+import { it, describe, expect, vi, assert, test } from "vitest";
 import { ServerlessRunningStages } from "../index";
+
+import {
+  CloudFormationClient,
+  paginateDescribeStacks,
+  Stack,
+  Tag,
+} from "@aws-sdk/client-cloudformation";
+
+vi.mock("CloudFormationClient");
+
+// test('mocked axios', async () => {
+//   await axios.get('string')
+
+//   expect(axios.get).toHaveBeenCalledWith('string')
+//   expect(axios.post).toBeUndefined()
+// })
+
+// test('can get actual axios', async () => {
+//   const ax = await vi.importActual<typeof axios>('axios')
+
+//   expect(vi.isMockFunction(ax.get)).toBe(false)
+// })
 
 describe("index test", () => {
   it("test function returns proper value", () => {
-    const workflowFunction = vi.fn(() => ServerlessRunningStages.test);
+    // const workflowFunction = vi.fn(
+    //   () => ServerlessRunningStages.getAllStagesForRegion
+    // );
 
-    workflowFunction();
+    // console.log(workflowFunction());
 
-    expect(workflowFunction).toReturnWith(1);
+    expect(
+      ServerlessRunningStages.getAllStagesForRegion("my-region")
+    ).toReturnWith([]);
   });
+});
+
+test("Math.sqrt()", () => {
+  expect(Math.sqrt(4)).toBe(2);
+  expect(Math.sqrt(144)).toBe(12);
+  expect(Math.sqrt(2)).toBe(Math.SQRT2);
+});
+
+test("JSON", () => {
+  const input = {
+    foo: "hello",
+    bar: "world",
+  };
+
+  const output = JSON.stringify(input);
+
+  expect(output).eq('{"foo":"hello","bar":"world"}');
+  assert.deepEqual(JSON.parse(output), input, "matches original");
+});
+
+it("foo", () => {
+  assert.equal(Math.sqrt(4), 2);
 });
