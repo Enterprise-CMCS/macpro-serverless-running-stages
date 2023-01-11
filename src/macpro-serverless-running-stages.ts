f53cd592acf9781f32f59733b5b45527311c00f1
Jon Holman <9025884+JonHolman@users.noreply.github.com>
2023-01-11 13:38:31 -0500

    Moved code over from cms-bigmac

--- src/macpro-serverless-running-stages.ts
+++ src/macpro-serverless-running-stages.ts
@@ -1,33 +1,48 @@
+import {
+  CloudFormationClient,
+  paginateDescribeStacks,
+  Stack,
+  Tag,
+} from "@aws-sdk/client-cloudformation";
 import * as readlineSync from "readline-sync";
 
-type Tag = {
-  Key: string;
-  Value: string;
-};
+function tagsListToTagDict(tagList: Tag[]) {
+  const retVal: any = {}; // TODO: remove any
+  for (const keyValPair of tagList) {
+    if (keyValPair.Key) {
+      retVal[keyValPair.Key] = keyValPair.Value;
+    }
+  }
+  return retVal;
+}
 
 export class ServerlessRunningStages {
-  private async getAllStacksForRegion(region: string) {
-    return [];
-  }
-
-  static get test() {
-    console.log("a1");
-    return 1;
-  }
-
-  private async getAllStacksForStage(
+  static async getAllStagesForRegion(
     region: string,
-    stage: string,
-    addFilters?: Tag[]
+    ignoreStages = ["master"]
   ) {
-    let stacks = await this.getAllStacksForRegion(region);
-    const matchTags = [
-      {
-        Key: "STAGE",
-        Value: stage,
-      },
-    ];
-    matchTags.push(...(addFilters || []));
-    return stacks;
+    const client = new CloudFormationClient({ region: region });
+    const stages: string[] = [];
+
+    for await (const page of paginateDescribeStacks({ client }, {})) {
+      if (page.Stacks) {
+        stages.push(
+          ...new Set(
+            page.Stacks.reduce((acc: string[], stack: Stack) => {
+              const tags = tagsListToTagDict(stack.Tags || []);
+              if (
+                tags["STAGE"] &&
+                tags["PROJECT"] === process.env.PROJECT &&
+                !ignoreStages.includes(tags["STAGE"])
+              ) {
+                acc.push(tags["STAGE"]);
+              }
+              return acc;
+            }, [])
+          )
+        );
+      }
+    }
+    return stages;
   }
 }
