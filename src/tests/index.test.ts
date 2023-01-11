2e3604b9c29f07e30ce81888cdde772980020634
Jon Holman <9025884+JonHolman@users.noreply.github.com>
2023-01-11 16:07:08 -0500

    Update index.test.ts

--- src/tests/index.test.ts
+++ src/tests/index.test.ts
@@ -1,11 +1,8 @@
-import { it, describe, expect, vi, assert, test, beforeEach } from "vitest";
+import { it, describe, expect, vi, beforeEach } from "vitest";
 import { ServerlessRunningStages } from "../index";
 import {
   CloudFormationClient,
-  paginateDescribeStacks,
   DescribeStacksCommand,
-  Stack,
-  //   Tag,
 } from "@aws-sdk/client-cloudformation";
 import { mockClient } from "aws-sdk-client-mock";
 
@@ -15,75 +12,51 @@ beforeEach(() => {
   cfnMock.reset();
 });
 
-describe("test paginate describe stacks command", () => {
-  it("describe stacks command returns proper value", async () => {
-    cfnMock
-      .on(DescribeStacksCommand, {
-        // NextToken: undefined,
-      })
-      .resolves({
-        NextToken: "1",
-        Stacks: [
-          {
-            StackName: "Bob",
-            CreationTime: new Date(0),
-            StackStatus: "Great",
-          },
-        ],
-      })
-      .on(DescribeStacksCommand, {
-        NextToken: "1",
-      })
-      .resolves({
-        Stacks: [
-          {
-            StackName: "Fred",
-            CreationTime: new Date(0),
-            StackStatus: "Good",
-          },
-        ],
-      });
+describe("getAllStagesForRegion command's tests", () => {
+  process.env.PROJECT = "myProject";
+  const sampleStackResponse = {
+    StackName: "Bob",
+    CreationTime: new Date(0),
+    StackStatus: "Great",
+  };
 
-    const client = new CloudFormationClient({});
-    // const stacks = await client.send(new DescribeStacksCommand({}));
-
-    const paginator = paginateDescribeStacks({ client }, {});
-
-    const stacks: Stack[] = [];
-    for await (const page of paginator) {
-      console.log("page:", page);
-      stacks.push(...(page.Stacks || []));
-    }
-
-    expect(stacks).toHaveLength(2);
-    expect(stacks[0]).toEqual({
-      StackName: "Bob",
-      CreationTime: new Date(0),
-      StackStatus: "Great",
+  it("happy path", async () => {
+    cfnMock.on(DescribeStacksCommand, {}).resolves({
+      Stacks: [
+        {
+          ...sampleStackResponse,
+          Tags: [
+            { Key: "PROJECT", Value: "myProject" },
+            { Key: "STAGE", Value: "myStage" },
+            { Key: "SERVICE", Value: "myService" },
+          ],
+        },
+      ],
     });
-    expect(stacks[1]).toEqual({
-      StackName: "Fred",
-      CreationTime: new Date(0),
-      StackStatus: "Good",
+    const workflowFunction = vi.fn((region) =>
+      ServerlessRunningStages.getAllStagesForRegion(region)
+    );
+
+    const runningStages = await workflowFunction("my-region");
+
+    expect(workflowFunction).toHaveBeenCalledWith("my-region");
+    expect(runningStages).toStrictEqual(["myStage"]);
+    expect(workflowFunction).toReturnWith(["myStage"]);
+    expect(runningStages).toHaveLength(1);
+  });
+
+  it("test without tags", async () => {
+    cfnMock.on(DescribeStacksCommand, {}).resolves({
+      Stacks: [sampleStackResponse],
     });
+    const workflowFunction = vi.fn((region) =>
+      ServerlessRunningStages.getAllStagesForRegion(region)
+    );
+
+    const runningStages = await workflowFunction("my-region");
+    expect(workflowFunction).toHaveBeenCalledWith("my-region");
+    expect(runningStages).toStrictEqual([]);
+    expect(workflowFunction).toReturnWith([]);
+    expect(runningStages).toHaveLength(0);
   });
 });
-
-// ############
-// const workflowFunction = vi.fn(
-//   () => ServerlessRunningStages.getAllStagesForRegion
-// );
-
-// expect(
-//   ServerlessRunningStages.getAllStagesForRegion("my-region")
-// ).toReturnWith([]);
-
-// const response = await cfnMock.send(new DescribeStacksCommand({}));
-
-// console.log("response:", response);
-// expect(response.Stacks).toHaveLength(2);
-// expect(response.Stacks?.[0]).toStrictEqual({
-//   CreationTime: new Date(),
-//   sk: "b",
-// });
-// ############
